4e548aefd8034e6a1d612d97b66b9de070ac3afa
Gabriel Pascua <io@gabrielpascua.com>
2020-07-18 20:29:53 -0400

    chartjs integration

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -6,19 +6,31 @@ import { Bar } from 'vue-chartjs'
 export default {
   extends: Bar,
   mounted () {
-    let chartdata = [];
-    for(let i=0,max=60; i<max; i++){
-        chartdata.push({
-            x: (i%10) === 0 ? `d${i+1}` : '',
-            y: Math.floor(Math.random() * Math.floor(20))
+    let barData = [];
+    let lineData = [];
+    let baseDate = Date.parse('2020-01-01');
+    let oneDay = 24 * 60 * 60 * 1000;
+    const maxValue = 2000;
+    for(let i=0,max=42; i<max; i++){
+        lineData.push(Math.random() * Math.floor(1000));
+        barData.push({
+            x: new Date(baseDate + (oneDay * i)),
+            y: Math.floor(Math.random() * Math.floor(2000))
         });
     };
 
-    let chartdataset = {
-        labels: chartdata.map(c => c.x),
+    let barDataset = {
+        labels: barData.map(c => c.x),
         datasets: [{
-            data: chartdata.map(c => c.y),
-            borderWidth: 1
+            data: barData.map(c => c.y),
+            borderWidth: 1,
+            backgroundColor: 'rgba(0,0,0,.125)'
+        },{
+            data: lineData,
+            type: 'line',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(228,228,0,.75)',
+            borderWidth: 2
         }]
     };
 
@@ -27,14 +39,35 @@ export default {
             xAxes: [{
                 gridLines: {
                     offsetGridLines: true
+                },
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        quarter: 'MMM YYYY',
+                        day: 'MMM DD'
+                    }
+                },
+                ticks: {
+                    maxRotation: 0,
+                    minRotation: 0,
+                    callback: function(value, index, values) {
+                        return (index % 8 === 0) ? value : '';
+                    }
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    stepSize: (maxValue / 5)
                 }
             }]
         },
-        responsive: true,
-        height: 300
+        legend: {
+            display: false
+        },
+        responsive: true
     };
 
-    this.renderChart(chartdataset, chartoptions);
+    this.renderChart(barDataset, chartoptions);
   }
 }
 </script>
