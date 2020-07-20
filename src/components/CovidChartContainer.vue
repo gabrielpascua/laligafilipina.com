0f3b177e99c73ea9b9d6dd83531853f32f8754db
Gabriel Pascua <io@gabrielpascua.com>
2020-07-20 07:05:27 -0400

    prod build fixes

--- src/components/CovidChartContainer.vue
+++ src/components/CovidChartContainer.vue
@@ -8,10 +8,10 @@
 </template>
 
 <script>
-import axios from 'axios';
-import CovidChart from './CovidChart';
+import axios from "axios";
+import CovidChart from "./CovidChart.vue";
 
-const dateParams = (function(){
+const dateParams = (function() {
   const now = new Date();
   const oneDayInMs = 24 * 60 * 60 * 1000;
   const endDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -21,60 +21,61 @@ const dateParams = (function(){
     lookbackDays: lookbackDays,
     oneDayInMs: oneDayInMs,
     startDate: new Date(endDate.getTime() - (lookbackDays * oneDayInMs))
-  }
+  };
 })();
 
-const getChartOptions = function(){
+const getChartOptions = function() {
   return {
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    offsetGridLines: true
-                },
-                type: 'time',
-                time: {
-                    displayFormats: {
-                        quarter: 'MMM YYYY',
-                        day: 'MMM DD'
-                    }
-                },
-                ticks: {
-                    maxRotation: 0,
-                    minRotation: 0,
-                    callback: function(value, index, values) {
-                        return (index % 3 === 0) ? value : '';
-                    }
-                }
-            }]
+    scales: {
+      xAxes: [{
+        gridLines: {
+          offsetGridLines: true
         },
-        legend: {
-            display: false
+        type: "time",
+        time: {
+          displayFormats: {
+            quarter: "MMM YYYY",
+            day: "MMM DD"
+          }
         },
-        responsive: true
-    };
-}
+        ticks: {
+          maxRotation: 0,
+          minRotation: 0,
+          callback: function(value, index, values) {
+            return (index % 3 === 0) ? value : "";
+          }
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true
+  };
+};
 
-const transformCovid19 = function(covidData){
-  let barData = [];
-  let lineData = [];
-  covidData.forEach((d, i) => {
-    let dt = new Date(dateParams.startDate.getTime() + (dateParams.oneDayInMs * i));
+const transformCovid19 = function(covidData) {
+  const barData = [];
+  const lineData = [];
+  for (let i = 0, max = covidData.length; i < max; i++) {
+    const d = covidData[i];
+    const dt = new Date(dateParams.startDate.getTime() + (dateParams.oneDayInMs * i));
     lineData.push(d.Recovered / d.Confirmed);
     barData.push({
       x: dt,
       y: d.Confirmed
     });
-  });
+  }
 
-  return { barData, lineData };
-}
+  return {barData:barData, lineData:lineData};
+};
 
 const transformNinja = function(covidData) {
-  let barData = [];
-  let lineData = [];
-  for(let i=0,max=dateParams.lookbackDays; i<max; i++){
-    let dt = new Date(dateParams.startDate.getTime() + (dateParams.oneDayInMs * i));
-    let key = `${dt.getMonth()+1}/${dt.getDate()}/${dt.getFullYear().toString().substring(2)}`;
+  const barData = [];
+  const lineData = [];
+  for (let i = 0, max = dateParams.lookbackDays; i < max; i++) {
+    const dt = new Date(dateParams.startDate.getTime() + (dateParams.oneDayInMs * i));
+    const key = `${dt.getMonth() + 1}/${dt.getDate()}/${dt.getFullYear().toString().substring(2)}`;
     lineData.push(covidData.timeline.recovered[key] / covidData.timeline.cases[key]);
     barData.push({
       x: dt,
@@ -82,38 +83,37 @@ const transformNinja = function(covidData) {
     });
   }
 
-  return { barData, lineData };
-}
+  return {barData:barData, lineData:lineData};
+};
 
-const setChartData = function(standardData){
-  const { barData, lineData } = standardData;
+const setChartData = function(standardData) {
   return {
-      labels: barData.map(c => c.x),
-      datasets: [{
-          data: barData.map(c => c.y),
-          borderWidth: 1,
-          backgroundColor: 'rgba(0,0,0,.125)'
-      },{
-          data: lineData,
-          type: 'line',
-          backgroundColor: 'transparent',
-          borderColor: 'rgba(228,228,0,.75)',
-          borderWidth: 2
-      }]
+    labels: standardData.barData.map((c) => c.x),
+    datasets: [{
+      data: standardData.barData.map((c) => c.y),
+      borderWidth: 1,
+      backgroundColor: "rgba(0,0,0,.125)"
+    }, {
+      data: standardData.lineData,
+      type: "line",
+      backgroundColor: "transparent",
+      borderColor: "rgba(228,228,0,.75)",
+      borderWidth: 2
+    }]
   };
-}
+};
 
 export default {
-  name: 'CovidChartContainer',
-  components: { CovidChart },
-  data: function(){
+  name: "CovidChartContainer",
+  components: {CovidChart},
+  data: function() {
     return {
       loaded: false,
       chartData: null,
       chartOptions: null
     };
   },
-  mounted: async function(){
+  mounted: async function() {
     this.loaded = false;
 
     const covid19api = `https://api.covid19api.com/country/philippines?from=${dateParams.startDate.toISOString()}&to=${dateParams.endDate.toISOString()}`;
@@ -123,23 +123,21 @@ export default {
     try {
       requestData = await axios.get(covid19api);
       if (!requestData) {
-        throw new Error('retry other source');
+        throw new Error("retry other source");
       }
     } catch (error) {
-      console.log(error);
       requestData = await axios.get(ninjaApi);
     }
-    
+
     requestData = requestData || {};
-    
-    const { data: rawData } = requestData;
+    const rawData = requestData.data;
     const covidData = Array.isArray(rawData) ? transformCovid19(rawData) : transformNinja(rawData);
 
     this.chartData = setChartData(covidData);
     this.chartOptions = getChartOptions();
     this.loaded = true;
   }
-}
+};
 </script>
 
 <style>
