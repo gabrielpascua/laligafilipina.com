f4970fdb71c70ad893e6babdfbb05b0a231b1dd7
Gabriel Pascua <io@gabrielpascua.com>
2020-07-25 08:29:17 -0400

    add charts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import DayName from "./components/DayName.vue";
 import CopyrightYear from "./components/CopyrightYear.vue";
+import CovidChartContainer from "./components/CovidChartContainer.vue";
 
 new Vue({
   el: "#app",
   components: {
     "day-name": DayName,
-    "copyright-year": CopyrightYear
+    "copyright-year": CopyrightYear,
+    "covid-chart-container": CovidChartContainer
   }
 });
