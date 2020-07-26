8fe9b4335dcdf623109559d45ea27179fe98bd8e
Gabriel Pascua <io@gabrielpascua.com>
2020-07-25 22:03:13 -0400

    Weather data component

---
+++ src/components/common/weather/Weather.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>
+    {{temperature}}&deg;C
+    <br />
+    {{description}}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    temperature: {
+      type: String
+    },
+    description: {
+      type: String
+    }
+  }
+};
+</script>
