9e96aa01941962db2f6e3cde290e17ad622d695b
Gabriel Pascua <io@gabrielpascua.com>
2020-07-25 20:42:29 -0400

    moving components to folders

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import DayName from "./components/DayName.vue";
-import CopyrightYear from "./components/CopyrightYear.vue";
-import CovidChartContainer from "./components/CovidChartContainer.vue";
+import DayName from "./components/common/DayName.vue";
+import CopyrightYear from "./components/common/CopyrightYear.vue";
+import CovidChartContainer from "./components/covid-chart/CovidChartContainer.vue";
 
 new Vue({
   el: "#app",
