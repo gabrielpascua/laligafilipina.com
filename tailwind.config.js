8ce31e9388c68608f831efa469196248796d748b
Gabriel Pascua <io@gabrielpascua.com>
2022-04-21 15:49:27 -0400

    header tailwind conversion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+// Default Configuration:
+// https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
 module.exports = {
     content: [
         "./src/components/**/*.{html,jsx}",
         "./site/**/*.html"
     ],
     theme: {
+        container: {
+            center: true,
+            padding: "0.5rem"
+        },
         extend: {},
     },
     plugins: [],
