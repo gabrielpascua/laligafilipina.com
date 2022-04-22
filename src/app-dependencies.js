d324c220b44f2cd1282cabf33d6dc22c515967e1
Gabriel Pascua <io@gabrielpascua.com>
2022-04-22 17:50:20 -0400

    home page on tailwind

--- src/app-dependencies.js
+++ src/app-dependencies.js
@@ -3,5 +3,4 @@
 // used for async-await, else "regeneratorRuntime error is not defined"
 // error will manifest
 import "regenerator-runtime/runtime";
-import "lazysizes";
 import "lite-youtube-embed/src/lite-yt-embed.js";
