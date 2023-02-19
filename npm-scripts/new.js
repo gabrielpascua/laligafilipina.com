1cc1ac560117e2e3696136394f6e6652f2f17886
Gabriel Pascua <io@gabrielpascua.com>
2023-02-19 09:26:54 -0500

    new issue script

---
+++ npm-scripts/new.js
@@ -0,0 +1,51 @@
+const fs = require('fs/promises');
+const path = require('path');
+const cheerio = require('cheerio');
+
+(async function(){
+    const ISSUES_PATH = path.join(process.cwd(), 'site/data/issues');
+    const issues = await fs.readdir(ISSUES_PATH);
+    const lastIssueIndex = issues.findLastIndex((jsonFile) => jsonFile.endsWith('.json'));
+    
+    const LAST_ISSUE_FILE = path.join(ISSUES_PATH, issues[lastIssueIndex]);
+
+    const lastIssue = JSON.parse(await fs.readFile(LAST_ISSUE_FILE));
+    
+    let { issue, hero, ticker, carousel } = lastIssue;
+
+    // New Issue
+    issue.number += 1;
+    issue.date = new Date((new Date(issue.date)).getTime() + (7 * 24 * 60 * 60 * 1000))
+
+    // Empty Hero
+    const $ = cheerio.load(hero.content);
+    $('a').attr('href', '#');
+    $('.hero-lead').text('TBD');
+    $('.hero-dek').text('TBD');
+    $('p > small').text('tbd.com')
+    hero.content = $('body').html();
+
+    // Empty Tickers
+    let { primary, secondary } = ticker;
+    Object.keys(primary).forEach(k => primary[k] = '');
+    secondary.forEach(sec => {
+        Object.keys(sec).forEach(s => sec[s] = '');
+    });
+
+    // Empty Carousel
+    carousel.forEach(car => {
+        Object.keys(car).forEach(c => car[c] = '');
+    });
+
+    const newIssuePath = path.join(ISSUES_PATH, `${issue.number}.json`);
+    await fs.writeFile(newIssuePath, JSON.stringify(
+        {
+            issue,
+            hero,
+            ticker,
+            carousel
+        },
+        null,
+        '    '
+    ));
+})();
