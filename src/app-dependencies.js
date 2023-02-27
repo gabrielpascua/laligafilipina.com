f5ed0c711ed42d2077097f761421fbdd0e609bfe
Gabriel Pascua <io@gabrielpascua.com>
2023-02-27 15:53:01 -0500

    intersection observer prototype

--- src/app-dependencies.js
+++ src/app-dependencies.js
@@ -4,3 +4,23 @@
 // error will manifest
 import "regenerator-runtime/runtime";
 import "lite-youtube-embed/src/lite-yt-embed.js";
+
+const options = {
+    root: null, // default, use viewport
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.75 
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            document.getElementById('top-nav').classList.remove('active');
+            document.getElementById('sub-nav').classList.remove('active');
+        } else {
+            document.getElementById('top-nav').classList.add('active');
+            document.getElementById('sub-nav').classList.add('active');
+        }
+    })
+  }, options);
+  
+observer.observe(document.querySelector('body'));
