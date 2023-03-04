654d97fb0c57a10f42fef4802a4805ded39e373f
gp <io@gabrielpascua.com>
2023-03-04 16:34:01 -0500

    initial 23009 draft

--- src/components/common/DayName.vue
+++ src/components/common/DayName.vue
@@ -1,11 +1,11 @@
 <template>
     <a href="https://time.is/Philippines">
-      {{ day }}<br />{{ date }}
+      {{ date }}<br />{{ day }}
     </a>
 </template>
 
 <script>
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 export default {
